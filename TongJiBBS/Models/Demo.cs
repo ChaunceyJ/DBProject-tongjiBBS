d73fcde4b128778aac9ac742bd4629516de54ab7
ChaunceyJ <33971563+ChaunceyJ@users.noreply.github.com>
2019-07-06 00:11:36 +0800

    Demo

---
+++ TongJiBBS/Models/Demo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Oracle.ManagedDataAccess.Client;
+using System.Collections;
+
+
+namespace TongJiBBS.Models
+{
+    public class Demo
+    {
+        private string user_id = "1752058";
+        private string name;
+        
+        public Hashtable get_name()
+        {
+            Hashtable ht = new Hashtable();
+            using (OracleConnection con = new OracleConnection(common.conString))
+            {
+                using (OracleCommand cmd = con.CreateCommand())
+                {
+                    try
+                    {
+                        con.Open();
+                        cmd.BindByName = true;
+
+                        //Use the command to display employee names from 
+                        // the EMPLOYEES table
+                        cmd.CommandText = "select USER_NAME from USER_1 where USER_ID = :id";
+
+                        // Assign id to the department number 50 
+                        OracleParameter id = new OracleParameter("id", user_id);
+                        cmd.Parameters.Add(id);
+
+                        //Execute the command and use DataReader to display the data
+                        OracleDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            //await context.Response.WriteAsync("Employee First Name: " + reader.GetString(0) + "\n");
+                            ht.Add("name", reader.GetString(0));
+                        }
+
+                        reader.Dispose();
+                    }
+                    catch (Exception ex)
+                    {
+                        //await context.Response.WriteAsync(ex.Message);
+                        ht.Add("error", ex.Message);
+                    }
+                }
+
+            }
+
+            return ht;
+        }
+    }
+}
---
+++ TongJiBBS/Models/common.cs
@@ -0,0 +1,8 @@
+﻿
+namespace TongJiBBS.Models
+{
+    public class common
+    {
+        public static string conString = "User Id=system;Password=1234;Data Source=localhost:1521/orcl;";
+    }
+}
